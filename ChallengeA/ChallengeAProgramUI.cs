3dc2ebc70320e251a040d1f54421c7ecab9e1a78
Sebastian Moore <sebastianxcxm@gmail.com>
2021-12-15 19:12:25 -0500

    Reworked checklist, need to push updates

--- ChallengeA/ChallengeAProgramUI.cs
+++ ChallengeA/ChallengeAProgramUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChallengeARepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace ChallengeA
 {
     public class ChallengeAProgramUI
     {
-        private readonly PocosAndRepos _pocosAndRepos = new PocosAndRepos();
+        private readonly MenuRepo _pocosAndRepos = new MenuRepo();
         public void Run()
         {
             InitialInfo();
@@ -23,8 +24,51 @@ namespace ChallengeA
                 Console.BackgroundColor = ConsoleColor.DarkCyan;
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.Clear();
-                Console.WriteLine();
+                Console.WriteLine("\n\n [Welcome to the Cafe] \n\n" +
+                    "Please select an option:\n" +
+                    "1. Add Item to Menu\n" +
+                    "2. View Entire Cafe Menu\n" +
+                    "3. Delete Menu Items\n" +
+                    "4. Exit");
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                        //add item
+                        //display entire menu
+                        //remove items from menu
+                        //exit application
+                    case "1":
+                        CreateItem();
+                        break;
+                    case "2":
+                        ShowEntireMenu();
+                        break;
+                    case "3":
+                        DeleteMenuItems();
+                        break;
+                    case "4":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, please select an option from 1-4.\n" +
+                            "Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
             }
         }
+        private void CreateItem()
+        {
+            
+        }
+        private void ShowEntireMenu()
+        {
+
+        }
+        private void DeleteMenuItems()
+        {
+
+        }
+
     }
 }
