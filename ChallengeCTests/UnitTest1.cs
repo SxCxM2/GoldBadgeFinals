e8d61477e8ac6cff7c8ee5910251986bdbeed785
Sebastian Moore <sebastianxcxm@gmail.com>
2021-12-22 21:37:08 -0500

    Final Push

--- ChallengeCTests/UnitTest1.cs
+++ ChallengeCTests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ChallengeCRepository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace ChallengeCTests
@@ -9,6 +10,27 @@ namespace ChallengeCTests
         [TestMethod]
         public void TestMethod1()
         {
+
+            //arrange
+
+            PocosAndRepos repo = new PocosAndRepos();
+
+            //act
+
+            repo.PopcornBags++;
+            repo.IceCream++;
+            repo.HotDogs++;
+            repo.VeggieBurgers++;
+            repo.Hamburgers++;
+
+            //assert
+
+            Assert.AreEqual(repo.PopcornBags, 1);
+            Assert.AreEqual(repo.IceCream, 1);
+            Assert.AreEqual(repo.HotDogs, 1);
+            Assert.AreEqual(repo.VeggieBurgers, 1);
+            Assert.AreEqual(repo.Hamburgers, 1);
+
         }
     }
 }
